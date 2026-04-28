13bed1eb7f39fe4089d60455dd2ddbd89f0dfaf8
alstonmendonca <alstondmendonca@gmail.com>
2026-04-28 13:50:57 +0530

    Add missing OfflineBanner component

---
+++ src/renderer/components/OfflineBanner.jsx
@@ -0,0 +1,7 @@
+export default function OfflineBanner() {
+  return (
+    <div className="bg-yellow-100 border-l-4 border-yellow-500 text-yellow-700 p-3 mb-4 rounded">
+      <p className="text-sm font-medium">You are currently offline. Some features may be unavailable.</p>
+    </div>
+  );
+}
